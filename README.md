7633cece8d73fbc9882f5f72c660fc4f72d29d94
Louis MARSAUD <31000538+lmrsd@users.noreply.github.com>
2019-04-01 10:17:46 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Render Test Tool for 3dsmax-Vray
 
 #Prerequisties
-
-• 3dsMax (Test on 3dsmax 2019)
-
-• Vray (Test on vrayNext only)
+Python
+3dsmax
+Vray
--- bin/rttGui.py
+++ bin/rttGui.py
@@ -25,24 +25,69 @@ class RttWidget(baseType, uiType) :
 		self.setupUi(self)
 		self.setUpConnections()
 		MaxPlus.AttachQWidgetToMax(self)
+		self.guiBindIcons()
 		self.storedSettings = dict()
 		self.setSettings = dict()
 
-
 	def setUpConnections(self):
+
 		self.openVfbBtn.clicked.connect(rtt.showlast)
-		self.optionBtn.clicked.connect(self.collectStates)
+		self.historyBtn.clicked.connect(rtt.openHistory)
 		#Prod Btn
-		self.prodFullBtn.clicked.connect(partial(self.launchRender,"full",size=1))
-		self.prodHalfBtn.clicked.connect(partial(self.launchRender,"full",size=0.5))
-		self.prodQuartBtn.clicked.connect(partial(self.launchRender,"full",size=0.25))
+		self.prodFullBtn.clicked.connect(partial(self.launchRender,"full",size=1,ipr=False))
+		self.prodHalfBtn.clicked.connect(partial(self.launchRender,"full",size=0.5,ipr=False))
+		self.prodQuartBtn.clicked.connect(partial(self.launchRender,"full",size=0.25,ipr=False))
 		#Draft Btn
-		self.draftFullBtn.clicked.connect(partial(self.launchRender,"draft",size=1))
-		self.darftHalfBtn.clicked.connect(partial(self.launchRender,"draft",size=0.5))
-		self.draftQuartBtn.clicked.connect(partial(self.launchRender,"draft",size=0.25))
+		self.draftFullBtn.clicked.connect(partial(self.launchRender,"draft",size=1,ipr=False))
+		self.draftHalfBtn.clicked.connect(partial(self.launchRender,"draft",size=0.5,ipr=False))
+		self.draftQuartBtn.clicked.connect(partial(self.launchRender,"draft",size=0.25,ipr=False))
+		#
+		self.iprBtn.clicked.connect(partial(self.launchRender,"draft",size=1,ipr=True))
 
 	def closeEvent(self,e):
 		return
+
+	def guiBindIcons(self):
+		imgrootDir = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
+		print imgrootDir
+		#Prod Full
+		prodFullIcon = QtGui.QIcon()
+		prodFullIconPath = os.path.join(imgrootDir,"gui","img","prodFull.png")
+		prodFullIcon.addPixmap(QtGui.QPixmap(prodFullIconPath))
+		self.prodFullBtn.setIcon(prodFullIcon)
+		#Prod Mid
+		prodMidIcon = QtGui.QIcon()
+		prodMidIconPath = os.path.join(imgrootDir,"gui","img","prodMid.png")
+		prodMidIcon.addPixmap(QtGui.QPixmap(prodMidIconPath))
+		self.prodHalfBtn.setIcon(prodMidIcon)
+		#Prod Quarter
+		prodQuarterIcon = QtGui.QIcon()
+		prodQuarterIconPath = os.path.join(imgrootDir,"gui","img","prodQuarter.png")
+		prodQuarterIcon.addPixmap(QtGui.QPixmap(prodQuarterIconPath))
+		self.prodQuartBtn.setIcon(prodQuarterIcon)
+		#Draft Full
+		draftFullIcon = QtGui.QIcon()
+		draftFullIconPath = os.path.join(imgrootDir,"gui","img","draftFull.png")
+		draftFullIcon.addPixmap(QtGui.QPixmap(draftFullIconPath))
+		self.draftFullBtn.setIcon(draftFullIcon)
+		#Draft Mid
+		draftMidIcon = QtGui.QIcon()
+		draftMidIconPath = os.path.join(imgrootDir,"gui","img","draftMid.png")
+		draftMidIcon.addPixmap(QtGui.QPixmap(draftMidIconPath))
+		self.draftHalfBtn.setIcon(draftMidIcon)
+		#Draft Quarter
+		draftQuarterIcon = QtGui.QIcon()
+		draftQuarterIconPath = os.path.join(imgrootDir,"gui","img","draftQuarter.png")
+		draftQuarterIcon.addPixmap(QtGui.QPixmap(draftQuarterIconPath))
+		self.draftQuartBtn.setIcon(draftQuarterIcon)
+		#IPR
+		iprIcon = QtGui.QIcon()
+		iprIconPath = os.path.join(imgrootDir,"gui","img","iprFull.png")
+		iprIcon.addPixmap(QtGui.QPixmap(iprIconPath))
+		self.iprBtn.setIcon(iprIcon)
+
+
+        
 
 	def collectStates(self):
 		for i in range(self.optionGlay.count()):
@@ -73,7 +118,7 @@ class RttWidget(baseType, uiType) :
 					rtt.getRenderOption(self.setSettings,disp=widget.isChecked())
 		print self.setSettings
 
-	def launchRender(self,setting,size=1):
+	def launchRender(self,setting,size=1,ipr=False):
 		#Render Settings Before
 		self.storedSettings = rtt.initPref()
 		# Ui states :
@@ -82,17 +127,33 @@ class RttWidget(baseType, uiType) :
 		if cmds.renderSceneDialog.isOpen():
 			cmds.renderSceneDialog.close()
 
-		rtt.changeRenderSettings(self.setSettings,setting)
+		rtt.changeRenderSettings(self.setSettings,setting,ipr)
 		cmds.renderWidth = size * self.storedSettings['outWidth']
 		cmds.renderHeight = size * self.storedSettings['outHeight']
+		#DisableSave
+		cmds.rendSaveFile = False
+
 		#Update rendersettings dialog ?
-		#renderSceneDialog.commit()
-		#renderSceneDialog.update()
+		cmds.renderSceneDialog.commit()
+		cmds.renderSceneDialog.update()
 		#
-		#MaxPlus.RenderExecute.QuickRender()
+
+		MaxPlus.RenderExecute.QuickRender()
+		#Save Bitmap current Render
+		bm = cmds.GetLastRenderedImage()
+		bm.filename =rtt.previewName(rtt.initPreviewDir())
+		cmds.save(bm)
+		cmds.close(bm)
+		
+		#Restore parameters
 		rtt.changeRenderSettings(self.setSettings,'default')
 		cmds.renderWidth = self.storedSettings['outWidth']
 		cmds.renderHeight = self.storedSettings['outHeight']
+		#EnableSave
+		cmds.rendSaveFile = False
+		cmds.renderSceneDialog.commit()
+		cmds.renderSceneDialog.update()
+
 
 def initRttGui():
 	#Check if exist and destroy if true
